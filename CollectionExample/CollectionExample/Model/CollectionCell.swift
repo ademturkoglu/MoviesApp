40524e829a4811aac3331e2511a8c65a9a829788
adem.turkoglu <>
2021-03-05 12:55:00 +0300

    added mainvc and views

---
+++ CollectionExample/CollectionExample/Model/CollectionCell.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionCell.swift
+//  CollectionExample
+//
+//  Created by Appinventiv on 27/02/18.
+//  Copyright © 2018 Appinventiv. All rights reserved.
+//
+
+import UIKit
+
+class CollectionCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+}
